51b628ba35b066c56cff70770c0c64ea53579c3e
cornelia chirila <cornelia.calarasiu@yahoo.it>
2020-10-26 14:29:07 +0000

    added the map

--- src/App.js
+++ src/App.js
@@ -1,18 +1,47 @@
 import React, { Component } from 'react';
+import GoogleMapReact from 'google-map-react';
+
 import FlatList from './components/FlatList';
-import flats from './flat.js';
-import Map from './components/Map';
+import Marker from './components/Marker';
+import flats from './flat';
 
 
 
 
 class App extends Component{
+  constructor(props){
+    super(props);
+    this.state={
+      selectedFlat: flats[0],
+      flats
+    }
+  }
+
+  center() {
+    return {
+      lat: this.state.selectedFlat.lat,
+      lng: this.state.selectedFlat.lng
+    };
+  }
+
+  selectFlat = (index) => {
+    this.setState({ selectedFlat: flats[index] });
+  }
+
   render(){
-    // flats.map(flat=>console.log(flat.name))
+    const { apartmentName }= this.state
     return (
       <div>
-        <FlatList flats={flats} />
-        <Map />
+        <FlatList
+          flats={this.state.flats}
+          selectedFlat={this.state.selectedFlat}
+          selectFlat={this.selectFlat}
+        />
+        <div className="map-container">
+          <GoogleMapReact defaultCenter={this.center()} defaultZoom={12}>
+            <Marker lat={this.state.selectedFlat.lat} lng={this.state.selectedFlat.lng} />
+          </GoogleMapReact>
+        </div>
       </div>
     );
   }
--- src/components/FlatList.js
+++ src/components/FlatList.js
@@ -1,13 +1,26 @@
 import React, { Component } from "react";
 import Flat from './Flat';
 
-const FlatList = ({flats})=>{
+const FlatList = (props)=>{
+  const renderList = () => {
+    return props.flats.map((flat, index) => {
+      return (
+        <Flat
+          flat={flat}
+          key={flat.lat}
+          selected={flat.name === props.selectedFlat.name}
+          index={index}
+          selectFlat={props.selectFlat}
+        />
+      );
+    });
+  };
+
   return (
-    <div className='flat-list'>
-       {flats.map(flat=><Flat name = {flat.name} key={flats.indexOf(flat)} price={flat.price + flat.priceCurrency} img={flat.imageUrl} className='card'/>)}
+    <div className="flat-list">
+      {renderList()}
     </div>
-   
-  )
+  );
 
 }
 
